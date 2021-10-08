746e881d3524c02a50f8622766c46f0692d32489
Vicky Liu <vickyl@ntdev.microsoft.com>
2021-10-08 05:14:06 -0700

    Add gitignore and create hello world dotnet console app

---
+++ buddychatcli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace buddychatcli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
