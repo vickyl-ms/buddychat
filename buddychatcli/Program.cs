d2c87f00bb6e997a9dd8d89b4edc90628e14224a
Vicky Liu <vickyl@ntdev.microsoft.com>
2021-10-12 12:31:09 -0700

    Add commandline parsing

--- buddychatcli/Program.cs
+++ buddychatcli/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using CommandLine;
+using System.Threading.Tasks;
 
-namespace buddychatcli
+namespace BuddyChatCLI
 {
     class Program
     {
-        static void Main(string[] args)
+        public static readonly string SignUpFileName = "signup.csv";
+        public static readonly string ParticipantsFileName = "participants.json";
+        public static readonly string PairingHistoryFileName = "pairing_history.json";
+
+        static int Test(BuddyChatCommand command)
+        {
+            Console.WriteLine("Command: " + command);
+            return 0;
+        }
+
+        static async Task<int> Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            return await Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .MapResult(async (CommandLineOptions opts) => 
+                {
+                    try 
+                    {
+                        return Test(opts.Command);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Exception thrown!");
+                        return -1;
+                    }
+                },
+                errs => Task.FromResult(-1));
         }
     }
 }
