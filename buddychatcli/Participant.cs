136deea73c7fb2c51b9ea9eef6bf9d59a4b322b1
Sagar Rajesh Vora <savor@microsoft.com>
2021-10-13 15:30:23 -0700

    Adding participant class structure

---
+++ buddychatcli/Participant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuddyChatCLI
+{
+    public class Participant
+    {
+        // uniquely identify a participant
+        public int participant_id { get; set; }
+        
+        // list of all the sessions the participant had participated
+        public List<int> session_participated { get; set; }
+        
+        // participant full name
+        public string name { get; set; }
+        
+        // participant email
+        public string email { get; set; }
+        
+        // participant data
+        public Dictionary<string, string> data { get; set; }
+    }
+
+}
+
