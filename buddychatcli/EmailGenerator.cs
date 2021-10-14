5355c65f4c1d5740386d8acf80c904df3e23b1d7
Israel Hernandez Leal <isleal@microsoft.com>
2021-10-14 12:08:50 -0700

    Use outputFolder instead of current directory

--- buddychatcli/EmailGenerator.cs
+++ buddychatcli/EmailGenerator.cs
@@ -1,46 +1,126 @@
-﻿using MsgKit;
+﻿using BuddyChatCLI;
+using MsgKit;
 using MsgKit.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using Message = MsgReader.Outlook.Storage.Message;
 
 namespace buddychatcli
 {
     public class EmailGenerator
     {
+        private string subject;
+        private string htmlBody;
+        private string outputFolder;
+
         /// <summary>
-        /// Generates the emails
+        /// Regex expression used to identify the first participant fields.
+        /// Note: HTML files are encoded and the less than / greater than symbols are encoded as &lt; and &gt; respectively.
         /// </summary>
-        /// <param name="templatePath">The path to the template in .oft format.</param>
-        public void GenerateEmails(string templatePath, List<Participant> participants)
+        private static readonly Regex FirstParticipantRegex = new Regex("&lt;participant1\\.(?<property>[\\w\\.]*)&gt;", RegexOptions.IgnoreCase);
+
+        /// <summary>
+        /// Regex expression used to identify the second participant fields.
+        /// Note: HTML files are encoded and the less than / greater than symbols are encoded as &lt; and &gt; respectively.
+        /// </summary>
+        private static readonly Regex SecondParticipantRegex = new Regex("&lt;participant2\\.(?<property>[\\w\\.]*)&gt;", RegexOptions.IgnoreCase);
+
+        public EmailGenerator(string templatePath, string outputFolder = null)
         {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            (this.subject, this.htmlBody) = ReadTemplate(templatePath);
 
-            Message msg = new Message("C:\\Users\\isleal\\Documents\\BuddyChatTemplate.oft");
+            if (string.IsNullOrEmpty(outputFolder))
+            {
+                outputFolder = Directory.GetCurrentDirectory();
+            }
 
-            string subject = msg.Subject;
-            string htmlBody = msg.BodyHtml;
+            this.outputFolder = outputFolder;
+        }
 
-            var utf = Encoding.UTF8;
-            var estEncoding = Encoding.GetEncoding(1252);
-            var newHtmlBody = utf.GetString(Encoding.Convert(estEncoding, utf, estEncoding.GetBytes(htmlBody)));
-
-            File.WriteAllTextAsync("C:\\Users\\isleal\\source\\test.html", htmlBody);
+        /// <summary>
+        /// Generates an email replacing the placeholders for each participant.
+        /// </summary>
+        /// <param name="templatePath">The path to the Outlook template in .oft format.</param>
+        /// <param name="participant1">The information about the first participant.</param>
+        /// <param name="participant2">The information about the second participant.</param>
+        public void GenerateEmail(Participant participant1, Participant participant2)
+        {
+            string newHtmlBody = ReplacePlaceholders(htmlBody, participant1, participant2);
 
             var email = new Email(null, subject);
-            email.Recipients.AddTo("vickyl@microsoft.com", "Vicky Liu");
-            email.Recipients.AddCc("savor@microsoft.com", "Sagar Rajesh");
+            email.Recipients.AddTo(participant1.email);
+            email.Recipients.AddTo(participant2.email);
             email.IconIndex = MessageIconIndex.UnreadMail;
 
             // The MsgKit library doesn't officially support creating Outlook templates (*.oft), it only supports *.msg files.
             // *.msg files can't be sent directly from Outlook so we would have to forward the message if we used this file type.
             // As a workaround, we can set BodyRtf and save the file as .oft. This creates a template file that we can open and send from Outlook.
             email.BodyRtf = GetEscapedRtf(newHtmlBody);
-            email.Save(@"C:\Users\isleal\source\newemail.oft");
+            email.Save(Path.Combine(this.outputFolder, $"{participant1.name} - {participant2.name}.oft"));
 
             Console.WriteLine("Emails have been created");
+        }
+
+        /// <summary>
+        /// Finds and replaces the placeholders in the HTML body.
+        /// </summary>
+        /// <param name="htmlBody">The HTML body of the template with placeholders in the form of <participant1.*>.</param>
+        /// <param name="participant1">The information about the first participant.</param>
+        /// <param name="participant2">The information about the second participant.</param>
+        /// <returns>The updated HTML body.</returns>
+        internal static string ReplacePlaceholders(string htmlBody, Participant participant1, Participant participant2)
+        {
+            htmlBody = ReplaceParticipantPlaceholders(htmlBody, FirstParticipantRegex, participant1);
+            htmlBody = ReplaceParticipantPlaceholders(htmlBody, SecondParticipantRegex, participant2);
+
+            return htmlBody;
+        }
+
+        internal static string ReplaceParticipantPlaceholders(string htmlBody, Regex regex, Participant participant)
+        {
+            JObject participantJOBject = JObject.Parse(JsonConvert.SerializeObject(participant));
+
+            foreach (Match match in regex.Matches(htmlBody))
+            {
+                string propertyName = match.Groups["property"].Value;
+                string value = string.Empty;
+
+                try
+                {
+                    value = participantJOBject.SelectToken(propertyName).ToString();
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine($"Property {propertyName} doesn't exist for Participant with email {participant.email}");
+                }
+
+                htmlBody = htmlBody.Replace(match.Value, value);
+            }
+
+            return htmlBody;
+        }
+
+        /// <summary>
+        /// Reads and extracts information from an Outlook template.
+        /// </summary>
+        /// <param name="templatePath">The path to the .otf file.</param>
+        /// <returns>The subject and the HTML body</returns>
+        internal static (string subject, string htmlBody) ReadTemplate(string templatePath)
+        {
+            // .otf files use encoding 1252 which is not supported by .net core unless we register CodePagesEncodingProvider
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            Message template = new Message(templatePath);
+
+            string subject = template.Subject;
+            string htmlBody = template.BodyHtml;
+
+            return (subject, htmlBody);
         }
 
         /// <summary>
--- buddychatcli/Program.cs
+++ buddychatcli/Program.cs
@@ -2,6 +2,7 @@
 using CommandLine;
 using System.Threading.Tasks;
 using buddychatcli;
+using System.Collections.Generic;
 
 namespace BuddyChatCLI
 {
@@ -23,8 +24,40 @@ namespace BuddyChatCLI
 
             Console.WriteLine("Command: " + options.Command);
 
-            EmailGenerator emailGenerator = new EmailGenerator();
-            emailGenerator.CreateItemFromTemplate();
+            Participant participant1 = new Participant()
+            {
+                email = "user1@microsoft.com",
+                name = "User1 LastName1",
+                data = new Dictionary<string, string>()
+                {
+                    { "introduction", "Hi" },
+                    { "pronouns", "He\\Him\\His" },
+                    { "question1", "Question 1 for participant 1" },
+                    { "answer1", "Answer 1 for participant 1" },
+                    { "question2", "Question 2 for participant 1" },
+                    { "answer2", "Answer 2 for participant 1" },
+                    { "question3", "Question 3 for participant 1" },
+                    { "answer3", "Answer 3 for participant 1" },
+                }
+            };
+
+            Participant participant2 = new Participant()
+            {
+                email = "user2@microsoft.com",
+                name = "User2 LastName2",
+                data = new Dictionary<string, string>()
+                {
+                    { "introduction", "Hi 2" },
+                    { "pronouns", "She\\Her\\Hers" },
+                    { "question1", "Question 1 for participant 2" },
+                    { "answer1", "Answer 1 for participant 2" },
+                    { "question2", "Question 2 for participant 2" },
+                    { "answer2", "Answer 2 for participant 2. No question 3 for this participant :)" }
+                }
+            };
+
+            EmailGenerator emailGenerator = new EmailGenerator("C:\\Users\\isleal\\Documents\\BuddyChatTemplate.oft");
+            emailGenerator.GenerateEmail(participant1, participant2);
 
             return 0;
         }
